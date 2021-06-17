cc28d6c6c7e0cf315d977ef71a293eccaea3a260
klauty <kaue.developer@gmail.com>
2021-06-17 11:16:30 -0300

    adicionando validação dos dados

--- src/main/java/com/example/demo/ExameController.java
+++ src/main/java/com/example/demo/ExameController.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,8 +39,39 @@ public class ExameController {
 	@PostMapping("/cadastarExame")
 	public String cadastrarExame(Exame exame, BindingResult result, Model model) {
 
+		// validando se o nome do paciente ja esta cadastrado no repositorio
+		Iterable<Exame> lista = exameRepository.findAll();
+		for (Exame item : lista) {
+			if (exame.getNome_paciente().equals(item.getNome_paciente())) {
+				System.out.println(item.getNome_paciente());
+				System.out.println("paciente ja cadastrado");
+				return "cadastrar-exame";
+			}
+		}
+
+		// verificando se a data do exame é valida
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+		System.out.println(exame.getData_exame());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			Date agora = sdf.parse(LocalDate.now().toString());
+			Date date_exame = sdf.parse(exame.getData_exame());
+
+			if (date_exame.compareTo(agora) < 0) {
+				System.out.println("data invalida !");
+				return "cadastrar-exame";
+			}
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		exameRepository.save(exame);
 		return "redirect:/index";
+
 	}
 
 	@GetMapping("/editar/{id}")
@@ -50,8 +86,28 @@ public class ExameController {
 	@PostMapping("/atualisar/{id}")
 	public String updateUser(@PathVariable("id") long id, Exame exame, BindingResult result, Model model) {
 
-		exameRepository.save(exame);
 
+		// verificando se a data do exame é valida
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+		System.out.println(exame.getData_exame());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			Date agora = sdf.parse(LocalDate.now().toString());
+			Date date_exame = sdf.parse(exame.getData_exame());
+
+			if (date_exame.compareTo(agora) < 0) {
+				System.out.println("data invalida !");
+				return "atualizar-exame";
+			}
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		exameRepository.save(exame);
 		return "redirect:/index";
 	}
 
